b566c7c2f76f8f16e6d7a974a50539d5290ecbfb
Wizzard Dev <gabriel.tofvesson@hotmail.com>
2016-11-22 19:55:25 +0100

    Added comment example

---
+++ Your first program/Comment.java
@@ -0,0 +1,9 @@
+public class Comment{
+  public static void main(String[] args){
+    System.out.println("Hello World");  // Single-line comment
+    /*
+      Multi-line
+      comment
+    */
+  }
+}
